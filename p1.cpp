dc5a4c593496f7b838746a74767894426749a254
BeautyKhatun786 <beauty721636@gmail.com>
2025-07-15 09:54:35 +0000

    p1-complete

---
+++ p1.cpp
@@ -0,0 +1,21 @@
+//to print memory address of five variables
+#include<stdio.h>
+int main(){
+	int a=10;
+	int b=20;
+	int c=30;
+	int d=40;
+	int e=50;
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+	printf("%d\n",e);
+
+	
+}
+
+
+//g++ first.cpp
+// .\a.exe
+//./a.out
